923e90e5eaffe1a2021a2779a401c91de5ab41ad
thatsm7md <Thatsm7md@gmail.com>
2024-03-30 14:17:06 +0400

    Chapter2exercises.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+# Quote from Dwayne Johnson
+Dwayne_quote = "One day, or day one."
+author = "Dwayne Jhonson"
+
+# Print the quote and author
+print(f"{author} once said, \"{Dwayne_quote}\"")
---
+++ Chapter 2- Variables & Comments/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+# Assigning a  favorite number to a variable
+fav_number = 8
+
+# Creating a message that reveals the favorite number
+message = f"My favorite number is {fav_number}!"
+
+
+print(message)
