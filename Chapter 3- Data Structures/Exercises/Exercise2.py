04c63586b56fa239019b9bc70b2e639e9c02906e
thatsm7md <Thatsm7md@gmail.com>
2024-04-01 02:49:40 +0400

    chapter3 exercises.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+# Storing the names of friends in a list
+names = ["Ahmed", "Eman", "Hashim", "Mustafa"]
+
+# Printing a personalized message for each friend
+for name in names:
+    print(f"Happy birthday {name}, enjoy your day!!!.")
