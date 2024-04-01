e79238cc46e1475e244743e7fde76c4474ff714c
thatsm7md <Thatsm7md@gmail.com>
2024-04-01 14:46:25 +0400

    Chapter4Exercises.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise11.py
@@ -0,0 +1,14 @@
+#Assigning a variable 
+alien_color = 'green'
+
+# using if statement to check the player choice
+if alien_color == 'green':
+ print("The player just earned 5 points.")
+
+#Assigning a variable
+alien_color = 'yellow'
+
+# using another if statement to check the player choice
+if alien_color == 'green':
+ 
+ print("The player just earned 5 points.")
